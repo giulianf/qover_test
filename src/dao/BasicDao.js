7f24edf646c70928582c2ec2831d70b3de657227
Julien <julien.fumanti@gmail.com>
2017-01-31 09:50:08 +0100

    Update BasicDao.js

--- src/dao/BasicDao.js
+++ src/dao/BasicDao.js
@@ -1,1 +1,37 @@
+import moment from 'moment';
 
+let winston = require('winston');
+winston.level = 'debug';
+
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {'timestamp':function() {return getLogDate(); },'colorize':true});
+
+const VERSION = 'qvr-1.0.0';
+
+export class BasicDao {
+
+    constructor() {
+    }
+    
+    info(message) {
+        winston.log('info', message);
+    }
+    
+    debug(message) {
+        winston.log('debug', message);
+    }
+    
+    error(message, error) {
+        winston.log('error', message, error);
+    }
+   
+    getLogDate() {
+        return moment().format('DD/MM/YYYY HH:mm:ss');
+    }
+
+    getVersion() {
+        return VERSION;
+    }
+
+    
+}
