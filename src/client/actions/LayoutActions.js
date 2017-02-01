12f9d55a2a6ce07eac851fa89fdbba36bd6353e5
Julien <julien.fumanti@gmail.com>
2017-02-01 13:44:19 +0100

    Update LayoutActions.js

--- src/client/actions/LayoutActions.js
+++ src/client/actions/LayoutActions.js
@@ -3,31 +3,6 @@ import ProvideConstants from '../constants/ProvideConstants';
 import ProvideService from '../services/ProvideService';
 
 export default {
-    /**
-     * get the profile from auth0
-     */
-    getProfileUser: (token, auth0, callback) => {
-        let profile;
-        let isAdmin;
-
-        auth0.getUserInfo(token, (error, userDetail) => {
-            if (error) {
-                callback(error);
-            } else {
-                profile = userDetail;
-
-                _.map(userDetail.app_metadata.roles, role => {
-                        if (_.isEqual(role, 'admin')) {
-                            isAdmin = true;
-                        }
-                });
-
-                dispatch(ProvideConstants.LOGIN_USER, {profile , isAdmin });
-                callback();
-            }
-        });
-    },
-
     logUserOut: () => {
         dispatch(ProvideConstants.LOGOUT_USER, {});
     }
