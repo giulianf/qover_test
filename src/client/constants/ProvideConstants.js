c7885ac2b155dee5a26ad7ddd6818e8f5cb05200
Julien <julien.fumanti@gmail.com>
2017-02-01 13:44:02 +0100

    Update ProvideConstants.js

--- src/client/constants/ProvideConstants.js
+++ src/client/constants/ProvideConstants.js
@@ -3,7 +3,6 @@ import keyMirror from 'keymirror';
 export default keyMirror({
     // Login
     DATA_ERROR : null,
-    LOGIN_USER : null,
     LOGOUT_USER : null,
     //CALCULATE
     CALCULATE:null,
