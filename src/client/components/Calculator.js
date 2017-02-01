83239dc8286192eeadcc55f7a439b9deddc3e904
Julien <julien.fumanti@gmail.com>
2017-02-01 10:57:01 +0100

    Update Calculator.js

--- src/client/components/Calculator.js
+++ src/client/components/Calculator.js
@@ -1,53 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes  } from 'react';
 import _ from 'lodash';
-import { Grid, Form, Row, Col, FormControl, FormGroup, ControlLabel, Button } from 'react-bootstrap';
+import { Grid, Form, Row, Col, FormControl, FormGroup, ControlLabel, Button, HelpBlock } from 'react-bootstrap';
 import Validator from '../../validator/validatorBasicInfo';
 import ProvideActions from '../actions/ProvideActions';
 
-export default class Calculator extends Component {
+class Calculator extends Component {
   constructor (props){
     super(props);
   }
 
   render () {
-      const validateName = Validator.validateNom(this.props.simulatorInfo.name) ? "success" : "error";
-      const validateCarValue = Validator.validateCarValue(this.props.simulatorInfo.name) ? "success" : "error";
+      if (_.isNil(this.props.simulatorInfo)) {
+          return null;
+      }
+      const validateNameBool = Validator.validateName(this.props.simulatorInfo.name) ;
+      const validateCarValueBool = Validator.validateCarValue(this.props.simulatorInfo.carValue);
+      const validateName = validateNameBool ? "success" : "error";
+      const validateCarValue = validateCarValueBool ? "success" : "error";
 
       const carOption = _.map(this.props.carOptionList, carName => {
           return (
-              <option value={carName.value}>carName.label</option>
+              <option key={carName.value} value={carName.value}>carName.label</option>
           )
       });
 
+      // Button disable
+    const disableCalculate = validateNameBool && validateCarValueBool ? false : true;
+    const buttonCalculate = (
+        <Button
+            bsStyle='primary'
+            disabled={ disableCalculate }
+            onClick={!disableCalculate ? this.props.handleSimulateClick : null}>
+            GET PRICE
+        </Button>
+    );
+
     return (
-        <Form>
+        <Form horizontal>
             <Grid>
                 <Row>
                    <Col lg={12}>
-                       <FormGroup controlId="formHorizontalNom" validationState={validateName}>
+                       <FormGroup controlId="formHorizontalName" validationState={validateName}>
                          <Col componentClass={ControlLabel} md={2} smHidden xsHidden>
                            Name
                          </Col>
-                         <Col sm={12} md={8}>
-                           <FormControl type="text" placeholder="Family namm"
-                               onChange={e => ProvideActions.updateBasicInfo({nom: e.target.value})}  value={this.props.simulatorInfo.name}/>
+                         <Col sm={6} md={4}>
+                           <FormControl type="text" placeholder="Family name"
+                               onChange={e => ProvideActions.updateSimulator({name: e.target.value})}  value={this.props.simulatorInfo.name}/>
+                               <HelpBlock>Name must higher than 5 characters</HelpBlock>
                          </Col>
                        </FormGroup>
-                       <FormGroup controlId="formControlsSelect">
-                          <ControlLabel>Select a car</ControlLabel>
-                          <FormControl componentClass="select" placeholder="Select a car" value={this.props.simulatorInfo.carName}>
-                            { carOption }
-                          </FormControl>
+                       <FormGroup controlId="formControlsCarSelect">
+                           <Col componentClass={ControlLabel} md={2} smHidden xsHidden>
+                             Select a car
+                           </Col>
+                           <Col sm={6} md={4}>
+                              <FormControl componentClass="select"
+                              onChange={e => ProvideActions.updateSimulator({carName: e.target.value})}
+                              placeholder="Select a car" value={this.props.simulatorInfo.carName}>
+                                { carOption }
+                              </FormControl>
+                          </Col>
                         </FormGroup>
-                        <FormGroup controlId="formHorizontalNom" validationState={validateCarValue}>
+                        <FormGroup controlId="formHorizontalCarValue" validationState={validateCarValue}>
                           <Col componentClass={ControlLabel} md={2} smHidden xsHidden>
                             Car Value
                           </Col>
-                          <Col sm={12} md={8}>
+                          <Col sm={6} md={4}>
                             <FormControl type="number" placeholder="Car value"
-                                onChange={e => ProvideActions.updateBasicInfo({nom: e.target.value})}  value={this.props.simulatorInfo.carValue}/>
+                                onChange={e => ProvideActions.updateSimulator({carValue: e.target.value})}  value={this.props.simulatorInfo.carValue}/>
                           </Col>
                         </FormGroup>
+                   </Col>
+                   <Col md={2}>
+                   { buttonCalculate }
                    </Col>
                 </Row>
             </Grid>
@@ -55,3 +81,9 @@ export default class Calculator extends Component {
     );
   }
 }
+
+Calculator.propTypes = {
+  handleSimulateClick: PropTypes.func.isRequired,
+};
+
+export default Calculator;
