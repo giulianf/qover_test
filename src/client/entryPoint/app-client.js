e901a01be1c46ca4a207a07f0132911c9e9acd42
Julien <julien.fumanti@gmail.com>
2017-02-01 08:39:29 +0100

    Fix react-tap-event-plugin issue

--- src/client/entryPoint/app-client.js
+++ src/client/entryPoint/app-client.js
@@ -5,13 +5,13 @@ import ReactDOM from 'react-dom';
 import AppRoutes from '../components/AppRoutes';
 import 'jquery';
 
-// var injectTapEventPlugin = require('react-tap-event-plugin');
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import './less/style.less';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-// injectTapEventPlugin();
+injectTapEventPlugin();
 
   //Needed for React Developer Tools
   window.React = React;
